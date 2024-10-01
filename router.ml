35290da9d896c88f5d94c740af2e6e555d53484d
pawan <150579399+isafwck@users.noreply.github.com>
2024-10-01 22:56:11 +0530

    Update router.ml

--- router.ml
+++ router.ml
@@ -1,3 +1,19 @@
-type route ={
+(* router.ml *)
 
+type route = {
+  path : string;
+  handler : unit -> string;
 }
+
+let routes = ref []
+
+let add_route path handler =
+  routes := { path; handler } :: !routes
+
+let find_handler path =
+  List.fold_left (fun acc route ->
+      if acc = None && route.path = path then
+        Some (route.handler)
+      else
+        acc
+    ) None !routes
